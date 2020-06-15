1720275812859fad151389bd6e8348204b5c8522
Mollie-S <olcha.olcha@gmail.com>
2020-06-15 17:25:49 +0200

    Added movement functionality to the hour and minute hands.

--- src/components/hands.js
+++ src/components/hands.js
@@ -13,11 +13,25 @@ function Hands() {
   }, []);
 
   const currentSecondsDegree = (date.getSeconds() / 60) * 360;
+  const currentMinutesDegree =
+    ((date.getMinutes() + date.getSeconds() * 60) / 60) * 360;
+  const currentHoursDegree =
+    ((date.getHours() + date.getMinutes() * 60) / 12) * 360;
 
   return (
     <div className="hands">
-      <div className="hand hour-hand"></div>
-      <div className="hand minute-hand"></div>
+      <div
+        className="hand hour-hand"
+        style={{
+          transform: `translateX(-50%) rotate(${currentHoursDegree}deg)`,
+        }}
+      ></div>
+      <div
+        className="hand minute-hand"
+        style={{
+          transform: `translateX(-50%) rotate(${currentMinutesDegree}deg)`,
+        }}
+      ></div>
       <div
         className="hand second-hand"
         style={{
